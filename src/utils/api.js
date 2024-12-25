b5f1de70f5ebacbb2fa446087ef06ea2c5897bf5
Sanket ramrao mule <149441904+sanket-67@users.noreply.github.com>
2024-12-25 18:17:14 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,88 +2,85 @@ const BASE_URL = "https://projecthub-38w5.onrender.com";
 
 class ApiError extends Error {
   constructor(status, message) {
-    super(message)
-    this.status = status
+    super(message);
+    this.status = status;
   }
 }
 
 export async function apiRequest(endpoint, options = {}) {
   const defaultOptions = {
-    credentials: "include",
+    credentials: "include", // Ensures cookies are sent with the request
     headers: {
       "Content-Type": "application/json",
     },
-  }
+  };
 
   const response = await fetch(`${BASE_URL}${endpoint}`, {
     ...defaultOptions,
     ...options,
-  })
+  });
 
-  const data = await response.json()
+  const data = await response.json();
 
   if (!response.ok) {
-    throw new ApiError(response.status, data.message || "Something went wrong")
+    throw new ApiError(response.status, data.message || "Something went wrong");
   }
 
-  return data
+  return data;
 }
 
 export const api = {
   // Auth endpoints
-  register: (formData) => 
+  register: (formData) =>
     fetch(`${BASE_URL}/users/register`, {
       method: "POST",
-      credentials: "include",
+      credentials: "include", // Includes cookies in the request
       body: formData, // Don't set Content-Type, browser will set it with boundary
-    }).then(res => res.json()),
+    }).then((res) => res.json()),
 
-  login: (data) => 
+  login: (data) =>
     apiRequest("/users/login", {
       method: "POST",
       body: JSON.stringify(data),
     }),
 
-  logout: () => 
+  logout: () =>
     apiRequest("/users/logout", {
       method: "POST",
     }),
 
   // Admin endpoints
-  verifyAdmin: () => 
+  verifyAdmin: () =>
     apiRequest("/users/admin", {
       method: "POST",
     }),
 
-  getPendingUsers: () => 
-    apiRequest("/users/pendingreq"),
+  getPendingUsers: () => apiRequest("/users/pendingreq"),
 
-  grantAccess: (data) => 
+  grantAccess: (data) =>
     apiRequest("/users/grantuser", {
       method: "POST",
       body: JSON.stringify(data),
     }),
 
-  banUser: (data) => 
+  banUser: (data) =>
     apiRequest("/users/bannedUser", {
       method: "POST",
       body: JSON.stringify(data),
     }),
 
   // Project endpoints
-  createProject: (data) => 
+  createProject: (data) =>
     apiRequest("/project/create", {
       method: "POST",
       body: JSON.stringify(data),
     }),
 
-  applyToProject: (data) => 
+  applyToProject: (data) =>
     apiRequest("/project/apply", {
       method: "POST",
       body: JSON.stringify(data),
     }),
 
-  getProjects: () => 
-    apiRequest("/project/list"),
-}
-
+  getProjects: () => apiRequest("/project/list"),
+};
